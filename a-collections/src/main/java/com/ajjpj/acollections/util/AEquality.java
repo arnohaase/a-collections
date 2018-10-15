b17feb3846cae9875f658cfce454a41b46043aeb
NFeiertag <NFeiertag@luxoft.com>
2018-10-15 14:52:50 +0200

    * Extending AEquality with 'notEquals'

--- a-collections/src/main/java/com/ajjpj/acollections/util/AEquality.java
+++ a-collections/src/main/java/com/ajjpj/acollections/util/AEquality.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 
 public interface AEquality {
     boolean equals(Object o1, Object o2);
+    default boolean notEquals(Object o1, Object o2){
+        return !equals(o1, o2);
+    }
     int hashCode(Object o);
 
     AEquality EQUALS = new AEquality() {
