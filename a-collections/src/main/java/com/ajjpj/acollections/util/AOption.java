c60453bfb797cac24ed95aa776b6fb8f269739ef
Arno Haase <arno.haase@haase-consulting.com>
2018-11-06 17:07:10 +0100

    AOption documentation complete

--- a-collections/src/main/java/com/ajjpj/acollections/util/AOption.java
+++ a-collections/src/main/java/com/ajjpj/acollections/util/AOption.java
@@ -1,9 +1,6 @@
 package com.ajjpj.acollections.util;
 
-import com.ajjpj.acollections.ACollection;
-import com.ajjpj.acollections.ACollectionBuilder;
-import com.ajjpj.acollections.AIterator;
-import com.ajjpj.acollections.AMap;
+import com.ajjpj.acollections.*;
 import com.ajjpj.acollections.immutable.AbstractImmutableCollection;
 import com.ajjpj.acollections.internal.ACollectionDefaults;
 import com.ajjpj.acollections.internal.ACollectionSupport;
@@ -20,12 +17,12 @@ import java.util.function.Supplier;
 
 
 /**
- * An {@code AOption} is a wrapper type, containing either one actual element or none. It represents a value that is either present or not,
+ * An {@code AOption} is a wrapper type, containing either one actual element or none. It represents a element that is either present or not,
  *  and it can be used instead of {@code null} in many cases.
  *
  * <p> AOption exists although Java's standard library contains {@link java.util.Optional}. The reason is that {@code Optional} has a very
  *  limited API and is intended for a very limited range of use cases, whereas {@code AOption} is designed for wide-spread use (e.g. as
- *  parameter type in APIs) and implements {@link ACollection}, giving it a rich API for working with its values.
+ *  parameter type in APIs) and implements {@link ACollection}, giving it a rich API for working with its elements.
  *
  * <p> The following examples illustrate some typical uses of {@code AOption} that go beyond simple wrapping / unwrapping:
  * <ul>
@@ -48,37 +45,92 @@ import java.util.function.Supplier;
  * <p> {@code AList<Person> persons = ...;}
  * <p> {@code AList<Address> addresses = persons.flatMap(this::addressFor);}
  *
- * <p><b>Null Handling:</b> AOption can hold {@code null} values and distinguishes between {@code some(null)} and {@code none()}. This
+ * <p><b>Null Handling:</b> AOption can hold {@code null} elements and distinguishes between {@code some(null)} and {@code none()}. This
  *  decision was made because it is a meaningful distinction in many situations:
  * <ul>
- *     <li> Some Map classes allow {@code null} values, in which case there is a difference between 'no entry for a given key' and 'a value
+ *     <li> Some Map classes allow {@code null} elements, in which case there is a difference between 'no entry for a given key' and 'an element
  *           of {@code null} for that key'.
- *     <li> In a JSON object, there is a formal difference between a key with value null and that key not being present
+ *     <li> In a JSON object, there is a formal difference between a key with element null and that key not being present
  *     <li> ...
  * </ul>
  *
- * We are aware that there some controversy as to whether an 'option' type should allow null values or not, and users of AOption may not
- *  want to use it that way. The above examples however are the reason why AOption allows values of {@code null}.
+ * We are aware that there some controversy as to whether an 'option' type should allow null elements or not, and users of AOption may not
+ *  want to use it that way. The above examples however are the reason why AOption allows elements of {@code null}.
  *
  * @param <T> the element type
  */
 public abstract class AOption<T> extends AbstractImmutableCollection<T> implements ACollectionDefaults<T, AOption<T>>, Serializable {
-
+    /**
+     * Returns the contained element (if any), throwing a NoSuchElementException if this AOption is empty.
+     * @throws NoSuchElementException if this AOption is empty
+     * @return the contained element
+     */
     public abstract T get();
+
+    /**
+     * Returns the contained element, or a default element if this AOption is empty.
+     * @param defaultValue the default element that is returned if this AOption is empty
+     * @return the contained element, or a default element if this AOption is empty
+     */
     public abstract T orElse(T defaultValue);
+
+    /**
+     * Returns the contained element, or the result of calling the {@link Supplier} passed in as a parameter. The supplier is called only
+     *  if this AOption is empty.
+     * @param f the supplier providing a default element if this AOption is empty.
+     * @return the contained element, or the result of calling the {@link Supplier} passed in as a parameter if this AOption is empty.
+     */
     public abstract T orElseGet(Supplier<T> f);
+
+    /**
+     * Returns the contained element, or null if this AOption is empty.
+     * @return the contained element, or null if this AOption is empty.
+     */
     public abstract T orNull();
+
+    /**
+     * Returns the contained element, or throws an exception provided by the {@link Supplier} passed in as a parameter. The supplier is called
+     *  only if this AOption is empty.
+     * @param exceptionSupplier is called if this AOption is empty, providing the exception that is then thrown.
+     * @param <X>               the exception's type that is thrown if this AOption is empty
+     * @return the contained element, if any
+     * @throws X if this AOption is empty
+     */
     public abstract <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X;
+
+    /**
+     * Returns the corresponding {@link Optional} for compatibility with other libraries
+     * @return the corresponding {@link Optional}
+     */
     public abstract Optional<T> toOptional();
 
+    /**
+     * Creates a new AOption for a given element, returning {@code some(o)} for non-null elements, and {@code none()} if a null element is passed
+     *  in.
+     * @param o   the element for which an AOption is created
+     * @param <T> the element's type
+     * @return {@code some(o)} for non-null elements, and {@code none()} if null is passed in
+     */
     public static <T> AOption<T> of(T o) {
         if (o != null) return some(o);
         else return none();
     }
 
+    /**
+     * Wraps a given element in an AOption, even if the element is {@code null}. This method never returns an empty AOption.
+     * @param o   the element that is wrapped in an AOption
+     * @param <T> the AOption's element type
+     * @return an AOption containing the element that was passed in.
+     */
     public static <T> AOption<T> some(T o) {
         return new ASome<>(o);
     }
+
+    /**
+     * Returns the empty AOption.
+     * @param <T> The result's element type
+     * @return the empty AOption
+     */
     public static <T> AOption<T> none() {
         //noinspection unchecked
         return (AOption<T>) ANone.INSTANCE;
@@ -88,33 +140,66 @@ public abstract class AOption<T> extends AbstractImmutableCollection<T> implemen
         return ACollectionSupport.toString(AOption.class, this);
     }
 
+    /**
+     * Returns true if and only if this AOption holds a element.
+     * @return true if and only if this AOption holds a element.
+     */
     public boolean isDefined() {
         return nonEmpty();
     }
+
+    /**
+     * Returns this AOption itself because that matches the inherited semantics of {@link ACollectionOps#headOption()}.
+     * @return this AOption itself
+     */
     @Override public AOption<T> headOption() {
         return this;
     }
 
+    /**
+     * Applies a function to this AOption's element (if any), wrapping it in a new AOption.
+     * @param f the function that is applied to the element
+     * @param <U> the result's element type
+     * @return an AOption containing the result of applying a function to this AOption's element.
+     */
     @Override public abstract <U> AOption<U> map (Function<T,U> f);
+
+    /**
+     * If called on an {@link AOption#none()}, this method returns {@link AOption#none()}. If this AOption contains an element, flatMap
+     *  calls the collection-valued function that is passed in on that element.
+     * <ul>
+     *     <li> If the result is empty, flatMap() returns {@link AOption#none()}.
+     *     <li> If the result contains exactly one element, flatMap() wraps that element in {@link AOption#some(Object)}.
+     *     <li> If the result contains more than one element, an IllegalArgumentException is thrown.
+     * </ul>
+     *
+     * @param f the function providing new element(s) for each element
+     * @param <U> the result's element type
+     * @return an AOption containing the function call's result only element
+     */
     @Override public <U> AOption<U> flatMap (Function<T, Iterable<U>> f) {
         //TODO optimized implementation
         return ACollectionSupport.flatMap(newBuilder(), this, f);
     }
+
     @Override public abstract <U> AOption<U> collect (Predicate<T> filter, Function<T, U> f);
 
+    /**
+     * Uses a predicate fo filter this AOption's element (if any), turning an {@link AOption#some(Object)} into {@link AOption#none()} if
+     *  the element does not pass the filter.
+     * @param f the predicate used as a filter
+     * @return the filtered AOption
+     */
     @Override public abstract AOption<T> filter (Predicate<T> f);
+
+    /**
+     * Uses a predicate fo filter this AOption's element (if any), turning an {@link AOption#some(Object)} into {@link AOption#none()} if
+     *  the element <b>passes</b> the filter.
+     * @param f the predicate used as a filter
+     * @return the filtered AOption
+     */
     @Override public AOption<T> filterNot (Predicate<T> f) {
         return ACollectionDefaults.super.filterNot(f);
-    }
-
-    @Override
-    public int size () {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty () {
-        return false;
     }
 
     @Override public <K> AMap<K, AOption<T>> groupBy (Function<T, K> keyExtractor) {
