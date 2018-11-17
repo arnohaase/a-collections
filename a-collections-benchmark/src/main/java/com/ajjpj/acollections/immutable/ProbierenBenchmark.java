50d0690c63d86277ddfd068f2e7639f61f7caf92
Arno Haase <arno.haase@haase-consulting.com>
2018-11-17 16:37:43 +0100

    cleanup benchmark

--- a-collections-benchmark/src/main/java/com/ajjpj/acollections/immutable/ProbierenBenchmark.java
+++ a-collections-benchmark/src/main/java/com/ajjpj/acollections/immutable/ProbierenBenchmark.java
@@ -8,30 +8,38 @@ import scala.math.Ordering;
 
 import java.util.Comparator;
 import java.util.Random;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 
-@Warmup(iterations = 1, time=10, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 2, time=10, timeUnit = TimeUnit.SECONDS)
 @BenchmarkMode(Mode.AverageTime)
-@Measurement(iterations = 3, time=5, timeUnit = TimeUnit.SECONDS)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(1)
+@Measurement(iterations = 5, time=8, timeUnit = TimeUnit.SECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(2)
 public class ProbierenBenchmark {
     private static final int size = 100_000;
     private static final int numIter = 10_000_000;
 
-
     @Benchmark
     public void testModifyAColl(Blackhole bh) {
-        final Random rand = new Random(12345);
         final Comparator<Integer> ordering = Comparator.naturalOrder();
         RedBlackTree.Tree<Integer,Integer> root = null;
 
-        for(int i=0; i<numIter; i++) {
-            final int key = rand.nextInt(size);
-            root = RedBlackTree.update(root, key, key, true, ordering);
-        }
+        final int key = 10;
+
+//        final TreeMap<Integer,Integer> map = new TreeMap<>();
+//        map.put(key, key);
+//        map.put(key, key);
+//        bh.consume(map);
+
+        root = RedBlackTree.update(root, key, key, true, ordering);
         bh.consume(root);
+        root = RedBlackTree.update(root, key, key, true, ordering);
+        bh.consume(root);
+//
+//
+//        bh.consume(RedBlackTree.update(RedBlackTree.update(null, 5L, 5L, true, Comparator.naturalOrder()), 5L, 5L, true, Comparator.naturalOrder()));
     }
 
     @Benchmark
